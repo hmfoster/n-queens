351c669dee5a98878d60915bc6d2d5d5ab9555a5
Hailey Foster <haileyhasmail@gmail.com>
2015-07-08 17:28:29 -0700

    First rook solution function passes.

--- src/solvers.js
+++ src/solvers.js
@@ -14,14 +14,56 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = new Board({'n':n}); //fixme
+  var rooks = n;
+  solution.togglePiece(0,0);
+  if (n === 1){
+    return solution.rows();
+  }
+  for (var i = 1; i < n; i++) {
+    for (var j = 1; j < n; j++) {
+      solution.togglePiece(i,j);
+      if(solution.hasAnyRowConflicts()){
+        solution.togglePiece(i,j);
+        break;
+      } else if(solution.hasAnyColConflicts()){
+        solution.togglePiece(i,j);
+      } else{
+        break;
+      }
+    }
+  }
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
-
-
-
+//create a board
+//toggle on 0x0
+//iterate over board starting with row[1]
+  //iterate over row
+    //toggle 1x0<--change
+    //check for row and column conflicts
+      //if yes, toggle off
+      //if no, break
+/*
+[1,0,0]
+[0,1,0]
+[0,0,1]
+*/
+/*
+[0,0,1]
+[0,1,0]
+[1,0,0]
+*/
+/*
+[0,1,0]
+[1,0,0]
+[0,0,1]
+*/
+/*
+[0,1,0]
+[0,0,1]
+[1,0,0]
+*/
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
